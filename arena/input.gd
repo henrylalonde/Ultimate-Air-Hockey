ee0cad7eb85d2e14b73a3826f07bcf1f5f0d0d84
henrylalonde <henryplalonde1@gmail.com>
2026-02-14 21:26:17 -0600

    escape key for fullscreen

--- arena/input.gd
+++ arena/input.gd
@@ -2,6 +2,12 @@ extends Node
 
 @onready var player1: Mallet = get_parent().get_node("Player1")
 
+
+func _process(_delta: float) -> void:
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
+
 func _physics_process(_delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	player1.desired_position = mouse_position
