ee346e4c4fca385c948ca76bf05b8a1801ac3795
henrylalonde <henryplalonde1@gmail.com>
2026-02-16 18:36:18 -0600

    menu separation and player2

--- arena/input.gd
+++ arena/input.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var player1: Mallet = get_parent().get_node("Player1")
+@onready var player2: Mallet = get_parent().get_node("Player2")
 
 
 func _process(_delta: float) -> void:
@@ -10,3 +11,8 @@ func _process(_delta: float) -> void:
 func _physics_process(_delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	player1.desired_position = mouse_position
+	player2.desired_position = Vector2(1500.0, 540.0)
+	
+# Desired functionality
+# player1.desired_position = RawInput.get_pointer_pos(0)
+# player2.desired_position = RawInput.get_pointer_pos(1)
