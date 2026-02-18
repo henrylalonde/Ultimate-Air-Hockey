a816bd7f6924ee9d8afca3e36f8143877cb4d1b1
henrylalonde <henryplalonde1@gmail.com>
2026-02-18 07:51:14 -0600

    dual mouse support

--- arena/input.gd
+++ arena/input.gd
@@ -3,16 +3,19 @@ extends Node
 @onready var player1: Mallet = get_parent().get_node("Player1")
 @onready var player2: Mallet = get_parent().get_node("Player2")
 
+var raw_mouse: RawMouse
+
+func _ready() -> void:
+	raw_mouse = RawMouse.new()
+	raw_mouse.set_p1_position(Vector2(551.0, 540.0))
+	raw_mouse.set_p2_position(Vector2(1384.0, 540.0))
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
 func _physics_process(_delta: float) -> void:
-	var mouse_position: Vector2 = get_viewport().get_mouse_position()
-	player1.desired_position = mouse_position
-	player2.desired_position = Vector2(1500.0, 540.0)
+	player1.desired_position = raw_mouse.get_p1_position()
+	player2.desired_position = raw_mouse.get_p2_position()
 	
-# Desired functionality
-# player1.desired_position = RawInput.get_pointer_pos(0)
-# player2.desired_position = RawInput.get_pointer_pos(1)
