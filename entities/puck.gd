d4af311188cde09bd702991efd5e2c8278d38c0d
henrylalonde <henryplalonde1@gmail.com>
2026-03-01 10:09:30 -0600

    sound effects!

--- entities/puck.gd
+++ entities/puck.gd
@@ -1,6 +1,11 @@
 extends RigidBody2D
 
+@onready var light_mallet_hit_sound: AudioStreamWAV = load("res://assets/wav/light_mallet_hit.wav")
+@onready var light_wall_hit_sound: AudioStreamWAV = load("res://assets/wav/light_wall_hit.wav")
+
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 var normal_sprite_scale := Vector2(0.25, 0.25)
 var large_sprite_scale := Vector2(0.3, 0.3)
 
@@ -12,3 +17,14 @@ func _ready() -> void:
 	tween.tween_property(sprite, "scale", normal_sprite_scale, 0.15)
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_OUT)
+
+
+func _on_body_entered(body: Node) -> void:
+	print(linear_velocity)
+	if body.is_in_group("wall"):
+		var volume_level: float = 4.2 * log(linear_velocity.length_squared() + 0.001) - 48.0
+		audio_stream_player.get_stream_playback().play_stream(light_wall_hit_sound, 0, volume_level)
+	elif body.is_in_group("mallet"):
+		var volume_level: float = 5 * log((linear_velocity - body.linear_velocity).length() + 0.001) - 30.0
+		audio_stream_player.get_stream_playback().play_stream(light_mallet_hit_sound, 0, volume_level)
+	
