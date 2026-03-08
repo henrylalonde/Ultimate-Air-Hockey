306e07bf0abcea4a80674bcb8753bfe8ecc615f0
henrylalonde <henryplalonde1@gmail.com>
2026-03-07 20:44:30 -0600

    fetching collision normal for more accurate dynamic sound effects

--- entities/puck.gd
+++ entities/puck.gd
@@ -9,6 +9,7 @@ extends RigidBody2D
 var normal_sprite_scale := Vector2(0.25, 0.25)
 var large_sprite_scale := Vector2(0.3, 0.3)
 
+var collision_sound_threshold: float = 50.0
 
 
 func _ready() -> void:
@@ -18,12 +19,27 @@ func _ready() -> void:
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_OUT)
 
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if state.get_contact_count() > 0:
+		for i in range(state.get_contact_count()):
+			var contact_normal = state.get_contact_local_normal(i)
+			var collider = state.get_contact_collider_object(i)
+			
+			if collider:
+				handle_collision(collider, contact_normal, state.linear_velocity)
 
-func _on_body_entered(body: Node) -> void:
+func handle_collision(body: Node, normal: Vector2, current_velocity: Vector2) -> void:
 	if body.is_in_group("wall"):
-		var volume_level: float = 4.2 * log(linear_velocity.length_squared() + 0.001) - 48.0
+		var impulse: float = abs(current_velocity.dot(normal))
+		if impulse < collision_sound_threshold:
+			return
+		var volume_level: float = 5 * log(impulse + 0.001) - 30.0
 		audio_stream_player.get_stream_playback().play_stream(light_wall_hit_sound, 0, volume_level)
+		
 	elif body.is_in_group("mallet"):
-		var volume_level: float = 5 * log((linear_velocity - body.linear_velocity).length() + 0.001) - 30.0
+		var mallet_vel = body.linear_velocity
+		var impulse: float = abs((current_velocity - mallet_vel).dot(normal))
+		if impulse < collision_sound_threshold:
+			return
+		var volume_level: float = 4.5 * log(impulse + 0.001) - 30.0
 		audio_stream_player.get_stream_playback().play_stream(light_mallet_hit_sound, 0, volume_level)
-	
