f6e2eca52dd827c0e854ad022cb35929c1633e79
henrylalonde <henryplalonde1@gmail.com>
2026-03-11 14:27:20 -0500

    rawmouse now autoload for consistency across scenes. also better hi hat on music

--- arena/player_input.gd
+++ arena/player_input.gd
@@ -9,29 +9,26 @@ var right_start := Vector2(1705.0, 540.0)
 var min_corner := Vector2(0.0, 0.0)
 var max_corner := Vector2(1920.0, 1080.0)
 
-var raw_mouse: RawMouse
-
 
 func _on_game_paused() -> void:
-	raw_mouse.freeze_input()
+	RawMouseAutoload.freeze_input()
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
 func _on_game_unpaused() -> void:
-	raw_mouse.unfreeze_input()
+	RawMouseAutoload.unfreeze_input()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _ready() -> void:
-	raw_mouse = RawMouse.new()
-	raw_mouse.set_p1_position(left_start)
-	raw_mouse.set_p2_position(right_start)
-	raw_mouse.set_p1_bound(min_corner.x, min_corner.y, max_corner.x / 2, max_corner.y)
-	raw_mouse.set_p2_bound(max_corner.x / 2, min_corner.y, max_corner.x, max_corner.y)
+	RawMouseAutoload.set_p1_position(left_start)
+	RawMouseAutoload.set_p2_position(right_start)
+	RawMouseAutoload.set_p1_bound(min_corner.x, min_corner.y, max_corner.x / 2, max_corner.y)
+	RawMouseAutoload.set_p2_bound(max_corner.x / 2, min_corner.y, max_corner.x, max_corner.y)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 
 func _physics_process(_delta: float) -> void:
-	player1.desired_position = raw_mouse.get_p1_position()
-	player2.desired_position = raw_mouse.get_p2_position()
+	player1.desired_position = RawMouseAutoload.get_p1_position()
+	player2.desired_position = RawMouseAutoload.get_p2_position()
 	
